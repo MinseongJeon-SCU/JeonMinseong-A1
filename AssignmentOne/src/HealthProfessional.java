2ec518736d5cbbd1ab9edcdb104a34c40293549f
MinseongJeon-SCU <minseong9078@gmail.com>
2024-11-06 19:40:26 +1000

    Create Objects of GP and Specialist

--- AssignmentOne/src/HealthProfessional.java
+++ AssignmentOne/src/HealthProfessional.java
@@ -2,7 +2,7 @@
 public class HealthProfessional {  
     private int id;
     private String name;
-    private String hospitalDepartment;
+    private String hospitalLocation;
 
     //Default Constructor
     public HealthProfessional () {     
@@ -10,19 +10,28 @@ public class HealthProfessional {
     }
 
     //Second Constructor that initialises all the instance variables
-    public HealthProfessional (int id, String name, String hospitalDepartment) {
+    public HealthProfessional (int id, String name, String hospitalLocation) {
         this.id = id;
         this.name = name;
-        this.hospitalDepartment = hospitalDepartment;
+        this.hospitalLocation = hospitalLocation;
     }
 
     // Method that prints all instance variables
     public void printAllInstanceVariables () {
         System.out.println("ID- " + id);
         System.out.println("Name- " + name);
-        System.out.println("Department- " + hospitalDepartment);
+        System.out.println("Department- " + hospitalLocation);
     }
 
+    //Create Objects of GP and Specialist
+    public static void main(String[] args) {
+        GeneralPractitioner GP1 = new GeneralPractitioner (1, "Minseong", "Brisbane", "Mental Health");
+        GeneralPractitioner GP2 = new GeneralPractitioner(2, "Jisung", "Gold Coast", "Vaccination");
+        GeneralPractitioner GP3 = new GeneralPractitioner(3, "Chanho", "Bilinga", "Family Planning");
+
+        Specialist SP1 = new Specialist (11, "Kim", "Coolangatta", "Cardiologist");
+        Specialist SP2 = new Specialist (22, "Park", "Suerfers Paradise", "Dermatologist");
+    }
 }
 
 //Child Class
@@ -35,9 +44,15 @@ class GeneralPractitioner extends HealthProfessional {
     }
 
     //Second constructor that initialises all the instance variables
-    public GeneralPractitioner (int id, String name, String hospitalDepartment, String gpType) {
-        super(id,name,hospitalDepartment);
+    public GeneralPractitioner (int id, String name, String hospitalLocation, String gpType) {
+        super(id,name,hospitalLocation);
         this.gpType = gpType;
+    }
+
+    @Override
+    public void printAllInstanceVariables() {
+        super.printAllInstanceVariables();
+        System.out.println("GP type - " + gpType);
     }
 }
 
@@ -51,10 +66,18 @@ class Specialist extends HealthProfessional {
     }
 
     //Second constructor that initialises all the instance variables
-    public Specialist (int id, String name, String hospitalDepartment, String specialistType) {
-        super(id,name,hospitalDepartment);
+    public Specialist (int id, String name, String hospitalLocation, String specialistType) {
+        super(id,name,hospitalLocation);
         this.specialistType = specialistType;
     }
 
+    @Override
+    public void printAllInstanceVariables() {
+        super.printAllInstanceVariables();
+        System.out.println("Specialist type - " + specialistType);
+    }
+    
 }
+
+
 
